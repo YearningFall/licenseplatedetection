2e6c35ed891eff89123ab0c876216c8b2fbc8d09
Krittaphas Tepsatidsil <78267407+YesterdayFall@users.noreply.github.com>
2023-05-26 16:59:57 +0700

    Reorganize files / Add example images

--- model/LicensePlateDetection.py
+++ model/LicensePlateDetection.py
@@ -2,7 +2,7 @@
 # so the program can find the source image
 from ultralytics.yolo.engine.model import YOLO
 
-Model = YOLO("best.pt")   
-Result = Model.predict(source='Driving in Bangkok Thailand 4k.mp4', show=True, save=True) #Display the video
-# change file name in source='' to detect plate in that file, don't forget about file type as well
-# after the program is done, you can find the results in the - folder
+Model = YOLO("best.pt") # Check README.md for best.pt download link, move it into this folder
+Result = Model.predict(source='./Source/27.png', show=True, save=True) 
+# change file name in source='' to detect plate in that file, don't forget about file type as well (.jpg, .png, .mp4)
+# after the program is done, you can find the results in the runs folder
